0f248bc92a0a7556c2dfb459d5c32e1164ef9075
Rico Nguyen <r.m.nguyen@students.uu.nl>
2019-03-12 00:14:20 +0100

    Update myScript.js

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -2,6 +2,17 @@ function myFunction() {
     document.getElementById("head").innerHTML = "The Wheel of Time";
 }
 
+// Copypasta van slides
+var img = document.getElementsByTagName('img'),
+img_len = img.length,
+i,
+alt;
+for (i=0; i < img_len; i++) {
+alt = img[i].getAttribute('alt');
+if (alt) {
+console.log(alt);
+}
+} 
 
 
 // Body geel maken testing kak pls send halp and nudes
