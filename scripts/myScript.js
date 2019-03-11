f84e6e8fabdecff412578d3f69aab298186204e6
Rico Nguyen <r.m.nguyen@students.uu.nl>
2019-03-12 00:12:08 +0100

    Idk what I'm doing halp
    Attempt selection droplist with javascript functionality

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -1,3 +1,13 @@
 function myFunction() {
     document.getElementById("head").innerHTML = "The Wheel of Time";
 }
+
+
+
+// Body geel maken testing kak pls send halp and nudes
+document.getElementById("body").onchange = function() {changeBackgroundColor()};
+
+function changeBackgroundColor() {
+for (let element of body.children) {
+    element.style.background = 'yellow';
+}
